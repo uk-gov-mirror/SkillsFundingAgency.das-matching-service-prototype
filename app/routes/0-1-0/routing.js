f26a9d2be4819c020289bbb173c3a4b6adf02606
jordanbullen <jordanbullendesign@Jordans-MacBook-Pro.local>
2018-12-04 14:04:31 +0000

    3 question flow

--- app/routes/0-1-0/routing.js
+++ app/routes/0-1-0/routing.js
@@ -2,9 +2,34 @@ module.exports = function(router, myData) {
 
     var version = "0-1-0";
 
-    // Route index page
-    // router.get('/' + version + '/bta-homepage', function (req, res) {
-    //   res.render(version + '/bta-homepage', {
-    //     myData: req.session.myData
-    //   });
+    // Start page
+    router.get('/' + version + '/start-page', function (req, res) {
+        res.render(version + '/start-page', {
+          })
+    });
+
+    router.post('/' + version + '/start-page', function (req, res){
+      res.redirect(301, './location');
+    });
+
+    // location
+    router.get('/' + version + '/location', function (req, res) {
+        res.render(version + '/location', {
+          })
+    });
+
+    router.post('/' + version + '/location', function (req, res){
+      res.redirect(301, './course');
+    });
+
+    // provider-results
+    router.get('/' + version + '/course', function (req, res) {
+        res.render(version + '/course', {
+          })
+    });
+
+    router.post('/' + version + '/course', function (req, res){
+      res.redirect(301, './provider-results');
+    });
+
 }
