1e8f65f68aadf4243625b3361340cf3a1a63368a
jordanbullen <jordanbullendesign@jordans-mbp.home>
2019-01-04 15:45:20 +0000

    service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Industry placement matching service',
+  serviceName: 'Find an industry placement',
 
   // Default port that prototype runs on
   port: '3000',
