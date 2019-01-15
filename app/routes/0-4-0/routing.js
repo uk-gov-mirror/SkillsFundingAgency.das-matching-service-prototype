13c1241592688574d823de680429e26fec8f827b
jordanbullen <jordanbullendesign@jordans-mbp.home>
2019-01-15 11:17:08 +0000

    radius removed from /search and dynamic dropdowns on results page

--- app/routes/0-4-0/routing.js
+++ app/routes/0-4-0/routing.js
@@ -127,7 +127,8 @@ module.exports = function(router, _myData) {
     });
 
     router.post('/' + version + '/provider-results', function (req, res){
-      req.session.route = req.body.route
+      req.session.route = req.body.route,
+      req.session.radius = req.body.radius
       if (req.body.resultsAction == "filter" && req.body.keyword != "") {
         res.redirect(301, './provider-results-filter');
       } else if (req.body.resultsAction == "filter" && req.body.keyword == "") {
